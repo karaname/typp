23b941f25ac6dde885de6d43e9a73e90ef6ea747
karaname <rekhov.ka@gmail.com>
2021-04-02 22:36:01 +0300

    fix warning / notes

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 typp: main.o texts.o
-	gcc `pkg-config ncursesw --libs --cflags` -g3 -o typp main.o texts.o -lncurses
+	gcc `pkg-config ncursesw --libs --cflags` -Wall -g3 -o typp main.o texts.o -lncurses
 
 main.o: main.c
-	gcc `pkg-config ncursesw --libs --cflags` -g3 -c main.c
+	gcc `pkg-config ncursesw --libs --cflags` -Wall -g3 -c main.c
 
 texts.o: texts.c
-	gcc `pkg-config ncursesw --libs --cflags` -g3 -c texts.c
+	gcc -Wall -g3 -c texts.c
--- texts.c
+++ texts.c
@@ -4,10 +4,11 @@
 
 wchar_t *generate_text(char *type_lang)
 {
-  srand(time(NULL));
-  if (strcmp(type_lang, "Русский") == 0) {
-    wchar_t *ru_arr[5];
-    ru_arr[0] = L"В 1901 году Тейлор снимала меблированные комнаты в мичиганском городе\n\
+  wchar_t *ru_arr[5];
+  wchar_t *en_arr[5];
+
+  /* russion texts */
+  ru_arr[0] = L"В 1901 году Тейлор снимала меблированные комнаты в мичиганском городе\n\
 Бей-Сити, когда ей попалась на глаза реклама международной Панамериканской\n\
 выставки, проходившей в эти дни в Буффало. Помимо прочего посетителям\n\
 предлагалось посетить находящийся неподалёку Ниагарский водопад.\n\
@@ -16,13 +17,13 @@ wchar_t *generate_text(char *type_lang)
 по порогам нижнего течения Ниагары, включая труднопроходимый водоворот на\n\
 повороте реки.";
 
-    ru_arr[1] = L"Первым известным прообразом сложной машины, как устройства для\n\
+  ru_arr[1] = L"Первым известным прообразом сложной машины, как устройства для\n\
 преобразования энергии из одного вида в другой, было наливное водяное колесо,\n\
 колесо, его с древнейших времён использовали для ирригации древние египтяне\n\
 и персы. Это механическое устройство служило для преобразования энергии\n\
 падающей воды (гидроэнергии) в энергию вращательного движения.";
 
-    ru_arr[2] = L"Основой устройства механической машины являются механизмы\n\
+  ru_arr[2] = L"Основой устройства механической машины являются механизмы\n\
 (например, кривошипно-шатунный механизм как часть паровой машины).\n\
 Внешне разные машины могут содержать подобные или схожие механизмы.\n\
 Но наиболее важные составляющие, остаются неизменными всегда, во всех\n\
@@ -33,7 +34,7 @@ wchar_t *generate_text(char *type_lang)
 передаточным механизмом, а механическое исполнительное устройство\n\
 — исполнительным механизмом.";
 
-    ru_arr[3] = L"Идею нового фильма предложил Яков Костюковский, прочитавший\n\
+  ru_arr[3] = L"Идею нового фильма предложил Яков Костюковский, прочитавший\n\
 в журнале «За рубежом» заметку о европейских мошенниках, использовавших для\n\
 перевозки ценностей гипсовые повязки, которые накладывались на месте мнимых\n\
 переломов. Эта сюжетная канва с перемещением основного действия в СССР\n\
@@ -42,49 +43,50 @@ wchar_t *generate_text(char *type_lang)
 Излагая концепцию будущей ленты, кинодраматурги отметили, что Юрий Никулин\n\
 сыграет в ней «скромного служащего отнюдь не героического вида и нрава»";
 
-    ru_arr[4] = L"В 1882 году на федеральном уровне был принят Акт об исключении\n\
+  ru_arr[4] = L"В 1882 году на федеральном уровне был принят Акт об исключении\n\
 китайцев, который серьёзно поражал китайцев в правах: по новому закону им\n\
 запрещалось как иммигрировать в США, так и проходить натурализацию в стране.\n\
 Более того, большинство китайцев более не могли воссоединиться со своими\n\
 близкими. В результате население нейборхуда не превышало 4000 человек вплоть\n\
 до Второй мировой войны.";
 
-    return ru_arr[rand() % 5];
-  }
-
-  if (strcmp(type_lang, "English") == 0) {
-    wchar_t *en_arr[5];
-    en_arr[0] = L"I never spend much time in school but I taught ladies plenty. It's true\n\
+  /* english texts */
+  en_arr[0] = L"I never spend much time in school but I taught ladies plenty. It's true\n\
 I hire my body out for pay, hey hey. I've gotten burned over Cheryl Tiegs,\n\
 blown up for Raquel Welch. But when I end up in the hay it's only hay,\n\
 hey hey. I might jump an open drawbridge, or Tarzan from a vine.\n\
 Cause I'm the unknown stuntman that makes Eastwood look so fine.";
 
-    en_arr[1] = L"This is my boss, Jonathan Hart, a self-made millionaire, he's quite a guy.\n\
+  en_arr[1] = L"This is my boss, Jonathan Hart, a self-made millionaire, he's quite a guy.\n\
 This is Mrs H., she's gorgeous, she's one lady who knows how to take care\n\
 of herself. By the way, my name is Max. I take care of both of them, which\n\
 ain't easy, cause when they met it was MURDER!";
 
-    en_arr[2] = L"Hong Kong Phooey, number one super guy. Hong Kong Phooey, quicker\n\
+  en_arr[2] = L"Hong Kong Phooey, number one super guy. Hong Kong Phooey, quicker\n\
 than the human eye. He's got style, a groovy style, and a car that just won't\n\
 stop. When the going gets tough, he's really rough, with a Hong Kong Phooey\n\
 chop (Hi-Ya!). Hong Kong Phooey, number one super guy. Hong Kong Phooey,\n\
 quicker than the human eye. Hong Kong Phooey, he's fan-riffic!";
 
-    en_arr[3] = L"Mutley, you snickering, floppy eared hound. When courage is needed,\n\
+  en_arr[3] = L"Mutley, you snickering, floppy eared hound. When courage is needed,\n\
 you're never around. Those medals you wear on your moth-eaten chest should be\n\
 there for bungling at which you are best. So, stop that pigeon, stop that\n\
 pigeon, stop that pigeon, stop that pigeon, stop that pigeon, stop that\n\
 pigeon, stop that pigeon. Howwww! Nab him, jab him, tab him, grab him, stop\n\
 that pigeon now.";
 
-    en_arr[4] = L"It took seven years of planning and 70,000 volunteers to make\n\
+  en_arr[4] = L"It took seven years of planning and 70,000 volunteers to make\n\
 everything go well. Many people have said that the organisation was not as\n\
 perfect as that of the Beijing Games, but there was a much better atmosphere\n\
 which spread out through the whole city. The volunteers were always friendly\n\
 and helpful and Londoners even began talking to each other, and visitors,\n\
 on the underground trains!";
 
+
+  srand(time(NULL));
+  if (strcmp(type_lang, "English") == 0) {
     return en_arr[rand() % 5];
+  } else {
+    return ru_arr[rand() % 5];
   }
 }
